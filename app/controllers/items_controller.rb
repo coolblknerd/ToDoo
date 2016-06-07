78ce6dd4d33e732cec667fd25a55067bc57085af
Reggie Davis <reginald.davis88@gmail.com>
2016-06-06 21:06:00 -0500

    Rerun Initial Commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,51 @@
+class ItemsController < ApplicationController
+  before_action :find_item, except: [:index, :new, :create]
+
+  def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to @item
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :description)
+  end
+
+end
